7ea8b8d12299efecb9bc409ef1ad754a07a3e886
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2015-04-23 16:29:39 -0500

    Added Script to start the application

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Welcome to the Oauth Connection Service\n');
 }).listen(port);
+
+//var express = require('express');
+//var app = express();
+
+//app.set('port', (process.env.PORT || 5000));
+//app.use(express.static(__dirname + '/public'));
+
+//app.get('/', function (request, response) {
+//    response.send('Hello World!');
+//});
+
+//app.listen(app.get('port'), function () {
+//    console.log("Node app is running at localhost:" + app.get('port'));
+//});
