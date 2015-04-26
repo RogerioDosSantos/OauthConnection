09e4a5b94600c060dba1eb1947a135feefd0d1e4
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2015-04-25 20:55:10 -0500

    Improved Logs and additional Authentication Test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿var Http = require("http");
 var URL = require("url");
 var QueryString = require('querystring');
-var Request = require('request');
+//var Request = require('request');
 var Map = require("collections/map");
 //var _oauth = require('oauth'), OAuth2 = OAuth.OAuth2;
 var Oauth2 = require('./ThirdParty/node-oauth/oauth2.js').OAuth2;
@@ -14,6 +14,51 @@ var _redirectUri = "https://oauth-connection.herokuapp.com";
 var _githubOauth = new Oauth2(_githubClient, _githubSecret, "https://github.com/",
     "login/oauth/authorize", "login/oauth/access_token", null /** Custom headers */);
 var _responseByID = new Map({});
+var _debugOptions = {};
+
+var LogType = {
+    error: 0,
+    warning: 1,
+    info: 2
+};
+
+function log(logType, level, description) {
+    if (_debugOptions.enableLog == 0)
+        return;
+    
+    var enabled = true;
+    if (_debugOptions.verbose == 0) {
+        switch (logType) {
+            case LogType.error:
+                enabled = _debugOptions.errorLevel != 0 && _debugOptions.errorLevel >= level;
+                break;
+
+            case LogType.warning:
+                enabled = _debugOptions.warningLevel != 0 && _debugOptions.warningLevel >= level;
+                break;
+
+            case LogType.info:
+                enabled = _debugOptions.infoLevel != 0 && _debugOptions.infoLevel >= level;
+                break;
+
+            default:
+                enabled = false;
+                break;
+        }
+    }
+
+    if (!enabled)
+        return;
+
+    var caller = log.caller;
+    caller = caller.name || "Unknown";
+
+    var message = "[Type: " + logType + ", Level: " + level + ", Function: " + caller + "] - " + description;
+    if (_debugOptions.console > 0) {
+        console.log(message);
+    }
+}
+
 
 function urlQueryToJson(query) {
     if (query == null)
@@ -35,17 +80,14 @@ function urlQueryToJson(query) {
 function queryStringToJson(request) {
     var url = URL.parse(request.url);
     return urlQueryToJson(url.query);
-    //var queryString = QueryString.parse(url.query);
-    //console.log(queryString);
-    //return {};
-
-    //return JSON.parse(queryString.jsonData);
 }
 
 function requestAuthentication(id, response) {
-    if (id == null)
+    if (id == null) {
+        log(LogType.error, 1, "Invalid id");
         return false;
-
+    }
+        
     var authURL = _githubOauth.getAuthorizeUrl({
         redirect_uri: _redirectUri,
         scope: ['repo', 'user'],
@@ -53,21 +95,23 @@ function requestAuthentication(id, response) {
     });
 
     _responseByID[id] = response;
-    Request(authURL, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var resp = _responseByID[id];
-            resp.end(body);
 
-            //console.log(body) // Show the HTML for the Google homepage.
-        }
-    });
+    //Request(authURL, function (error, response, body) {
+    //    if (!error && response.statusCode == 200) {
+    //        var resp = _responseByID[id];
+    //        resp.end(body);
+    //    }
+    //});
+
+    response.writeHead(301, { Location: authURL });
+    response.end();
 
     return true;
 }
 
 function sendResponse(response, result) {
     var resp = JSON.stringify(result);
-    console.log("Response: " + resp);
+    log(LogType.info, 5, "Response: " + resp);
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write(resp);
     response.end();
@@ -77,9 +121,35 @@ function sendError(response, errorCode, errorDescription) {
     sendResponse(response, {
         error: {
             code: errorCode,
-            descripton: errorDescription
+            description: errorDescription
         }
     });
+}
+
+function parseValue(value, defaultValue) {
+    var resp = parseInt(value);
+    if (isNaN(resp))
+        return defaultValue;
+
+    return resp;
+}
+
+function debugOptions(options, response) {
+    options = options || {};
+    _debugOptions["enableLog"] = parseValue(options.enableLog, _debugOptions.enableLog || 0);
+    _debugOptions["verbose"] = parseValue(options.verbose, _debugOptions.verbose || 0);
+
+    _debugOptions["console"] = parseValue(options.console, _debugOptions.console || 1);
+
+    _debugOptions["errorLevel"] = parseValue(options.errorLevel, _debugOptions.errorLevel || 0);
+    _debugOptions["warningLevel"] = parseValue(options.warningLevel, _debugOptions.warningLevel || 0);
+    _debugOptions["infoLevel"] = parseValue(options.infoLevel, _debugOptions.infoLevel || 0);
+
+    if (_debugOptions.errorLevel == 0 && options.errorLevel == null && options.enableLog != null && options.enableLog)
+        _debugOptions.errorLevel = 1;
+
+    if (response != null)
+        sendResponse(response, _debugOptions);
 }
 
 function getPath(request, index) {
@@ -98,8 +168,10 @@ function getPath(request, index) {
 }
 
 function test(id, response) {
-    if(id == null)
+    if (id == null) {
+        log(LogType.error, 1, "Invalid id");
         return false;
+    }
 
     /**
      * Creating an anchor with authURL as href and sending as response
@@ -120,20 +192,31 @@ function test(id, response) {
     return true;
 }
 
-function requestAuthenticationPhase1(id, authCode) {
-    if (id == null || authCode == null)
+function requestAuthenticationPhase1(options) {
+    if (options == null) {
+        log(LogType.error, 1, "Invalid options");
         return false;
+    }
+
+    var id = options.state;
+    if (id == null) {
+        log(LogType.error, 1, "Invalid id");
+        return false;
+    }
+
+    var response = _responseByID[id];
+    if (response == null) {
+        log(LogType.error, 1, "Could not find response: id = " + id);        
+        return false;
+    }
+
+
 
     /** Obtaining access token */
     oauth2.getOAuthAccessToken(
         authCode,
         { 'redirect_uri': _redirectUri },
         function (error, accessToken, refresh_token, results) {
-            var response = _responseByID[id];
-            if (response == null){
-                console.log("Could not find response: id = " + id);
-                return;
-            }
 
             if (error) {
                 sendError(response, -2, error);
@@ -144,7 +227,7 @@ function requestAuthenticationPhase1(id, authCode) {
                 sendResponse(response, results);
             }
 
-            //console.log('Obtained accessToken: ', accessToken);
+            log(LogType.info, 1, "Obtained accessToken: " + accessToken);
             sendResponse(response, {
                 accessToken: accessToken
             });
@@ -181,7 +264,11 @@ Http.createServer(function (request, response) {
             break;
 
         case "code":
-            error = !requestAuthenticationPhase1(options.id, options.code);
+            error = !requestAuthenticationPhase1(options);
+            break;
+
+        case "DebugOptions":
+            error = !debugOptions(options, response);
             break;
 
         case "Test":
@@ -247,6 +334,7 @@ Http.createServer(function (request, response) {
    
 }).listen(_port);
 
+debugOptions(null, null);
 console.log("Server Running - Port: " + _port);
 
 
