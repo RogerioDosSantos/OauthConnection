712dc74118e8c129fdc7c173a85a73b74041f2f6
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2015-05-01 17:09:31 -0500

    Introduced Evernote Authentication

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 ﻿var Http = require("http");
 var URL = require("url");
-var QueryString = require('querystring');
-//var Request = require('request');
+var QueryString = require("querystring");
 var Map = require("collections/map");
 var Q = require("q");
-//var _oauth = require('oauth'), OAuth2 = OAuth.OAuth2;
-var Oauth2 = require('./ThirdParty/node-oauth/oauth2.js').OAuth2;
+//var Oauth1 = require("./ThirdParty/node-oauth/oauth.js").OAuth;
+var Oauth2 = require("./ThirdParty/node-oauth/oauth2.js").OAuth2;
+var Evernote = require('evernote').Evernote;
 
-var _port = process.env.PORT || 8080;
+//Server Environment Variables
+var PORT = process.env.PORT || 8080;
+var SERVER_URL = process.env.SERVER_URL || "http://localhost:" + PORT;  //TODO: Put the SERVER_URL to be https://oauth-connection.herokuapp.com
+var GITHUB_CLIENT = process.env.GITHUB_CLIENT || "Your client";
+var GITHUB_SECRET = process.env.GITHUB_SECRET || "Your secret";
+var EVERNOTE_CLIENT = process.env.EVERNOTE_CLIENT || "Your client";
+var EVERNOTE_SECRET = process.env.EVERNOTE_SECRET || "Your secret";
+var EVERNOTE_USESANDBOX = process.env.EVERNOTE_USESANDBOX || false;
+
 var _providerConfigByType = new Map({});
 var _userConfigByAppNameIdKey = new Map({})
 var _tokenById = new Map({});
@@ -87,17 +95,37 @@ function Error(level, description) {
     log(LogType.error, level, description, caller);
 }
 
+function toEnumerator(enumaratorType, value) {
+    var tempValue = value || enumaratorType.rankLast;
+    tempValue = parseInt(tempValue, 10);
+    if (tempValue > enumaratorType.rankLast || tempValue < 0)
+        tempValue = enumaratorType.rankLast;
+
+    var values = Object.values(enumaratorType);
+    return values[tempValue];
+}
+
 function populateApplicationConfig() {
-    var githubClient = process.env.GITHUB_CLIENT || "04c56461b9d1392277dd";
-    var githubSecret = process.env.GITHUB_SECRET || "db3bd6e7393d2910b9f62bbb5c845e9b767ab784";
     _providerConfigByType.set(ProviderType.github, {
         name: "github",
-        user: githubClient,
-        secret: githubSecret,
-        redirectUri: "https://oauth-connection.herokuapp.com/Github",
-        oauth2: new Oauth2(githubClient, githubSecret, "https://github.com/",
+        user: GITHUB_CLIENT,
+        secret: GITHUB_SECRET,
+        redirectUri: SERVER_URL + "/Github",
+        oauth2: new Oauth2(GITHUB_CLIENT, GITHUB_SECRET, "https://github.com/",
         "login/oauth/authorize", "login/oauth/access_token", null /** Custom headers */)
     })
+
+    _providerConfigByType.set(ProviderType.evernote, {
+        name: "evernote",
+        user: EVERNOTE_CLIENT,
+        secret: EVERNOTE_SECRET,
+        redirectUri: SERVER_URL + "/Evernote",
+        envernoteClient: new Evernote.Client({
+            consumerKey: EVERNOTE_CLIENT,
+            consumerSecret: EVERNOTE_SECRET,
+            sandbox: EVERNOTE_USESANDBOX
+        })
+    }); 
 }
 
 function setToken(tokenId, token) {
@@ -165,25 +193,71 @@ function getProviderConfig(providerType) {
 }
 
 function getAuthorizeUrl(providerType, id) {
+    var deferred = Q.defer();
     var providerConfig = getProviderConfig(providerType);
     if (providerConfig == null) {
-        log(LogType.error, 5, "Could not get provider configuration");
-        return "";
+        deferred.reject(new Error(5, "Could not get provider configuration"));        
+        return deferred.promise;
     }
 
     if (id == null) {
-        log(LogType.error, 1, "Invalid id");
-        return "";
+        deferred.reject(new Error(1, "Invalid id"));
+        return deferred.promise;
     }
 
-    return providerConfig.oauth2.getAuthorizeUrl({
-        redirect_uri: providerConfig.redirectUri,
-        scope: ['repo', 'user'],
-        state: id
-    });
+    switch (providerType) {
+        case ProviderType.evernote:
+            providerConfig.envernoteClient.getRequestToken(providerConfig.redirectUri + "?state=" + id,
+                function (error, oauthToken, oauthTokenSecret, results) {
+                    //We are using the token by id map to hold the temporary token secret.
+                    setToken(id, oauthTokenSecret);                    
+                    var url = providerConfig.envernoteClient.getAuthorizeUrl(oauthToken);
+                    deferred.resolve({
+                        url: url
+                    });
+                });
+
+            break;
+
+        case ProviderType.github:
+            deferred.resolve({
+                url: providerConfig.oauth2.getAuthorizeUrl({
+                    redirect_uri: providerConfig.redirectUri,
+                    scope: ['repo', 'user'],
+                    state: id
+                })
+            });
+
+            break;
+
+        default:
+            log(LogType.error, 1, "Invalid provider");
+            break;
+    }
+
+    return deferred.promise;
 }
 
-function getAccessToken(providerType, authenticationCode) {
+function handleAccessCodeAnswer(deferred, error, accessToken, tempToken, results) {
+    if (error) {
+        deferred.reject(new Error(2, error));
+        return;
+    }
+
+    if (results.error) {
+        deferred.reject(new Error(2, results));
+        return;
+    }
+
+    deferred.resolve({
+        result: results,
+        accessToken: accessToken
+    });
+
+    log(LogType.info, 1, "Obtained accessToken: " + accessToken);
+}
+
+function getAccessToken(providerType, tempToken, tempSecret, authenticationCode) {
     var deferred = Q.defer();
     var providerConfig = getProviderConfig(providerType);
     if (providerConfig == null) {
@@ -196,32 +270,51 @@ function getAccessToken(providerType, authenticationCode) {
         return deferred.promise;
     }
 
-    if (providerConfig.oauth2 == null) {
-        deferred.reject(new Error(-1, "Invalid provider config."));
-        return deferred.promise;
+    switch (providerType) {
+        case ProviderType.evernote:
+            providerConfig.envernoteClient.getAccessToken(tempToken, tempSecret, authenticationCode,
+                function (error, accessToken, refresh_token, results) {
+                    handleAccessCodeAnswer(deferred, error, accessToken, refresh_token, results);
+                });
+            break;
+
+        case ProviderType.github:
+            providerConfig.oauth2.getOAuthAccessToken(authenticationCode,
+                { 'redirect_uri': providerConfig.redirectUri },
+                function (error, accessToken, refresh_token, results) {
+                    handleAccessCodeAnswer(deferred, error, accessToken, refresh_token, results);
+                });
+
+
+            //providerConfig.oauth2.getOAuthAccessToken(
+            //    authenticationCode,
+            //    { 'redirect_uri': providerConfig.redirectUri },
+            //    function (error, accessToken, refresh_token, results) {
+            //        if (error) {
+            //            deferred.reject(new Error(2, error));
+            //            return;
+            //        }
+
+            //        if (results.error) {
+            //            deferred.reject(new Error(2, results));
+            //            return;
+            //        }
+
+            //        deferred.resolve({
+            //            result: results,
+            //            accessToken: accessToken
+            //        });
+
+            //        log(LogType.info, 1, "Obtained accessToken: " + accessToken);
+            //    });
+
+            break;
+
+        default:
+            deferred.reject(new Error(1, "Invalid provider"));
+            break;
     }
 
-    providerConfig.oauth2.getOAuthAccessToken(
-        authenticationCode,
-        { 'redirect_uri': providerConfig.redirectUri },
-        function (error, accessToken, refresh_token, results) {
-            if (error) {
-                deferred.reject(new Error(2, error));
-                return;
-            }
-
-            if (results.error) {
-                deferred.reject(new Error(2, results));
-                return;
-            }
-
-            deferred.resolve({
-                result: results,
-                accessToken: accessToken
-            });
-
-            log(LogType.info, 1, "Obtained accessToken: " + accessToken);
-        });
 
     return deferred.promise;
 }
@@ -296,7 +389,8 @@ function sendResponse(response, result) {
 
 function requestToken(options, response) {
     options = options || {};
-    if (options.providerType == null) {
+    var providerType = toEnumerator(ProviderType, options.providerType);
+    if (providerType >= ProviderType.rankLast) {
         log(LogType.error, 1, "Invalid provider");
         return false;
     }
@@ -306,10 +400,10 @@ function requestToken(options, response) {
         log(LogType.error, 5, "Could not get user config");
         return false;
     }
-
-    var tokenId = userConfig.getTokenId(options.providerType);
+    
+    var tokenId = userConfig.getTokenId(providerType);
     if (tokenId == "") {
-        log(LogType.error, 1, "Provider not supported. Provider: " + options.providerType);
+        log(LogType.error, 1, "Provider not supported. Provider: " + providerType);
         return false;
     }
 
@@ -345,21 +439,28 @@ function requestAuthenticationPhase0(options, response) {
         return false;
     }
 
-    var id = userConfig.getTokenId(options.providerType);
+    var providerType = toEnumerator(ProviderType, options.providerType);
+    var id = userConfig.getTokenId(providerType);
     if (id == "") {
-        log(LogType.error, 1, "Could not get token id. Provider: " + options.providerType);
+        log(LogType.error, 1, "Could not get token id. Provider: " + providerType);
         return false;
     }
 
     setReturnUrl(id, options.returnUrl);
 
-    var authURL = getAuthorizeUrl(options.providerType, id);
-    if (!authURL) {
-        log(LogType.error, 5, "Could not get authentication URL.");
-        return false;
-    }
+    getAuthorizeUrl(providerType, id)
+    .then(function (result) {
+        if (!result.url) {
+            log(LogType.error, 1, "Could not get authentication URL. Detail: " + error);
+            return;
+        }
 
-    sendRedirection(response, authURL);
+        sendRedirection(response, result.url);
+    })
+    .fail(function (error) {
+        log(LogType.error, 1, "Could not get authentication URL. Detail: " + error);
+    });
+
     return true;
 }
 
@@ -404,9 +505,14 @@ function getPath(request, index) {
     return path[0];
 }
 
-function requestAuthenticationPhase1(options, response) {
+function requestAuthenticationPhase1(options, response, providerType) {
     if (options == null) {
         log(LogType.error, 1, "Invalid options");
+        return false;
+    }
+
+    if (providerType >= ProviderType.rankLast) {
+        log(LogType.error, 1, "Could not find provider type!");
         return false;
     }
 
@@ -421,14 +527,46 @@ function requestAuthenticationPhase1(options, response) {
         return false;
     }
 
-    var authCode = options.code;
-    if (authCode == null) {
-        sendError(response, -3, "Could not find authentication code!");
+    var returnUrl = getReturnUrl(id);
+    if (returnUrl == "") {
+        //log(LogType.error, 1, "Could not find the return URL!");
+        //return false;
+    }
+
+    var tempToken = null;
+    var tempSecret = null;
+    var authenticationCode = null;
+    switch (providerType) {
+        case ProviderType.evernote:
+            authenticationCode = options.oauth_verifier;
+            tempToken = options.oauth_token;
+            if (tempToken == null) {
+                log(LogType.error, 1, "Could not find temp token!");
+                return false;
+            }
+
+            tempSecret = getToken(id);
+            if (tempToken == null) {
+                log(LogType.error, 1, "Could not find temp token secret!");
+                return false;
+            }
+
+            break;
+
+        case ProviderType.github:
+            authenticationCode = options.code;
+            break;
+        default:
+            log(LogType.error, 1, "Invalid Provider Type!");
+            return false;
+    }
+
+    if (authenticationCode == null) {
+        log(LogType.error, 1, "Could not find authentication code!");
         return true;
     }
 
-    var returnUrl = getReturnUrl(id);
-    getAccessToken(ProviderType.github, authCode)
+    getAccessToken(providerType, tempToken, tempSecret, authenticationCode)
         .then(function (result) {
             setToken(id, result.accessToken);
             if (returnUrl != "") {
@@ -454,18 +592,118 @@ function requestAuthenticationPhase1(options, response) {
     return true;
 }
 
+//var _tempToken = "";
+//var _tempSecret = "";
 function test(options, response) {
 
+    //console.log(options);
+    var providerType = toEnumerator(ProviderType, options.providerType);
+    //var keys = Object.keys(ProviderType);
+    //var values = Object.values(ProviderType);
+    //console.log("Keys = " + keys);
+    //console.log("values = " + values);
+    //providerType = parseInt(providerType, 10);
+    //console.log("providerType = " + providerType);
 
-    //var returnUrl = getReturnUrl(options.state);
-    //sendRedirection(response, returnUrl);
 
-    //sendRedirection(response, "chrome-extension://dlodcfnfnmkeemfkifamaoapgdjbmgbe/Pages/githnote.html");
-    //sendRedirection(response, "chrome-extension://dlodcfnfnmkeemfkifamaoapgdjbmgbe");
-    //sendRedirection(response, "https://www.google.com/");
+    //providerType = keys[providerType];
+    //providerType = values[providerType];
+    console.log("providerType = " + providerType);
 
-    sendResponse(response, {
-    });
+    switch (providerType) {
+        case ProviderType.github:
+            sendResponse(response, {
+                provider: "Github"
+            });
+            break;
+
+        case ProviderType.evernote:
+            sendResponse(response, {
+                provider: "Evernote"
+            });
+            break;
+
+        default:
+            sendResponse(response, {
+                provider: "Invalid Provider"
+            });
+            break;
+    }
+
+    //var state = options.state || "0";
+    //console.log("Test - State: " + state);
+
+    //if (_envernoteClient == null) {
+    //    _envernoteClient = new Evernote.Client({
+    //        consumerKey: "roger-3576",
+    //        consumerSecret: "aadb5fffc9cf4617",
+    //        sandbox: true
+    //    });
+    //}
+    
+    //switch (state) {
+    //    case "0":           
+    //        _envernoteClient.getRequestToken("http://localhost:8080/Test?state=1",
+    //            function (error, oauthToken, oauthTokenSecret, results) {
+    //                // store tokens in the session
+    //                _tempToken = oauthToken;
+    //                _tempSecret = oauthTokenSecret;
+    //                // and then redirect to client.getAuthorizeUrl(oauthToken)
+    //                var url = _envernoteClient.getAuthorizeUrl(oauthToken);
+    //                sendRedirection(response, url);
+    //            });
+    //        break;
+
+    //    case "1":
+    //        console.log("Temp Token: " + _tempToken);
+    //        console.log("Temp Secret: " + _tempSecret);
+    //        console.log("Oauth Token:" + options.oauth_token);
+    //        console.log("Oauth Verifier:" + options.oauth_verifier);
+
+    //        _envernoteClient.getAccessToken(_tempToken, _tempSecret, options.oauth_verifier,
+    //            function (error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    //                sendResponse(response, {
+    //                    token: oauthAccessToken,
+    //                    oauthAccessTokenSecret: oauthAccessTokenSecret,
+    //                });
+    //            });                
+    //        break;
+
+    //    default:
+    //        sendError(response, -1, "Invalid State");
+
+    //}
+
+
+    //var providerConfig = getProviderConfig(ProviderType.evernote);
+    //if (providerConfig == null) {
+    //    log(LogType.error, 5, "Could not get provider configuration");
+    //    return "";
+    //}
+
+
+    ////this._performSecureRequest(oauth_token, oauth_token_secret, "GET", url, null, "", null, callback);  
+    ////function( oauth_token, oauth_token_secret, method, url, extra_params, post_body, post_content_type,  callback )
+
+    //https://sandbox.evernote.com/oauth?oauth_callback=http://www.foo.com&oauth_consumer_key=sample-api-key-4121&oauth_nonce=3166905818410889691&oauth_signature=T0+xCYjTiyz7GZiElg1uQaHGQ6I=&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1429565574&oauth_version=1.0
+
+    //providerConfig.oauth1.get(
+    //  "https://sandbox.evernote.com/oauth",
+    //  providerConfig.user,
+    //  providerConfig.secret,
+    //  function (e, data, res){
+    //      if (e) {
+    //          sendResponse(response, {
+    //              error: e
+    //          });
+    //          return;
+    //      }
+    //      var token = require('util').inspect(data);
+    //      sendResponse(response, {
+    //          token: token
+    //      });
+    //  });
+
 
     return true;
 }
@@ -504,7 +742,11 @@ Http.createServer(function (request, response) {
             break;
 
         case "Github":
-            error = !requestAuthenticationPhase1(options, response);
+            error = !requestAuthenticationPhase1(options, response, ProviderType.github);
+            break;
+
+        case "Evernote":
+            error = !requestAuthenticationPhase1(options, response, ProviderType.evernote);
             break;
 
         case "DebugOptions":
@@ -530,8 +772,8 @@ Http.createServer(function (request, response) {
 
     return;
 
-}).listen(_port);
+}).listen(PORT);
 
 populateApplicationConfig();
 debugOptions(null, null);
-console.log("Server Running - Port: " + _port);
+console.log("Server Running - Url: " + SERVER_URL);
